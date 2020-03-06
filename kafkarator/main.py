f79a17b0f4ba4200306556a72549fc038ab1a6b3
Morten Lied Johansen <morten.lied.johansen@nav.no>
2020-03-06 15:19:54 +0100

    Enable prometheus metrics
    Fixes #10

--- kafkarator/main.py
+++ kafkarator/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+from prometheus_client import Counter
+from starlette_exporter import PrometheusMiddleware, handle_metrics
 
 app = FastAPI()
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", handle_metrics)
+
+TEST_COUNTER = Counter("test_counter", "A counter to test that the prometheus integration works")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/count")
+async def count(n: int = 1):
+    TEST_COUNTER.inc(n)
+    return {"message": f"Increased counter by {n}"}
 
 
 @app.get("/isHealthy")
