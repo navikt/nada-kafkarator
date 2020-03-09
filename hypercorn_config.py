5b1f6ef1fc96a830f097f13ce93b581964e7787a
Morten Lied Johansen <morten.lied.johansen@nav.no>
2020-03-09 21:59:54 +0100

    Let's not do debugging in production

--- hypercorn_config.py
+++ hypercorn_config.py
@@ -1,7 +1,6 @@
 import logging
 
 bind = "0.0.0.0:8080"
-log_level = logging.DEBUG
-debug = True
+log_level = logging.INFO
 accesslog = logging.getLogger("hypercorn.access")
 errorlog = logging.getLogger("hypercorn.error")
